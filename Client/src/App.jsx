06da083a92ac0c0d09c52ff4db621dd5115506cb
Vaibhav_07 <vaibhavsrivastava0710@gmail.com>
2025-06-01 15:57:36 +0530

    backend url, login and register page changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { Routes, Route } from 'react-router-dom' 
+import { ToastContainer} from 'react-toastify';
 
 import Home from './pages/Home'
 import Result from './pages/Result'
@@ -16,6 +17,7 @@ const App = () => {
 
   return (
     <div className='pxx-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <ToastContainer position='bottom-right'/>
       <Navbar/>
       {showLogin && <Login />}
       <Routes>
--- Client/src/context/AppContext.jsx
+++ Client/src/context/AppContext.jsx
@@ -1,19 +1,51 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios"
 
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
-    const [user, setUser] = useState(null);
-    const [showLogin, setShowLogin] = useState(false);
+const [user, setUser] = useState(null);
+const [showLogin, setShowLogin] = useState(false);
+const [token, setToken] = useState(localStorage.getItem('token'))
+const [credit, setCredit] = useState(false)
 
-    const value = {
-        user, setUser, showLogin, setShowLogin
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+const loadCreditsData = async () => {
+    try {
+        const {data} = await axios.get(backendUrl + 'api/user/credits', {headers: {token}})
+        if(data.success){
+            setCredit(data.credits)
+            setUser(data.user)
+        }
+    } catch (error) {
+        console.error(error)
+        toast.error(error.message)
     }
+}
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
+const logout = () => {
+    localStorage.removeItem('token')
+    setToken('')
+    setUser(null)
+}
+
+useEffect(() => {
+    if(token){
+        loadCreditsData()
+    }
+}, [token])
+
+const value = {
+    user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credit, setCredit, loadCreditsData, logout
+}
+
+return (
+<AppContext.Provider value={value}>
+    {props.children}
+    </AppContext.Provider>
     )
 }
 export default AppContextProvider
